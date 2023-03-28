8e0ea13d60c0009b8804169c1d376c54229fe1d7
Samir Madhavan <samir.madhavan@gmail.com>
2023-03-28 14:37:32 +0530

    Add a new home page

---
+++ pages/game.js
@@ -0,0 +1,165 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+import styles from 'kfgame/styles/Home.module.css'
+import React, { useState, useEffect } from 'react';
+import { initialLevels, generateLevel, investorNames, businessNames } from '../components/levels';
+
+const inter = Inter({ subsets: ['latin'] })
+
+
+
+export default function Home() {
+  const [levelIndex, setLevelIndex] = useState(0);
+  const [levels, setLevels] = useState(initialLevels);
+  const [selectedInvestor, setSelectedInvestor] = useState();
+  const [totalScore, setTotalScore] = useState(0);
+  const [timer, setTimer] = useState(60); // Change the timer value based on your game requirements
+  const { smallBusiness, potentialInvestors } = levels[levelIndex];
+  const [selectedInvestorStatus, setSelectedInvestorStatus] = useState(
+    Array(potentialInvestors.length).fill(false)
+  );
+  const [gameOver, setGameOver] = useState(false);
+
+
+  const handleInvestorSelection = (investor) => {
+    setSelectedInvestor(investor);
+      const score = calculateScore(investor);
+      setTotalScore(totalScore + score);
+      handleNextLevel();
+  };
+
+   const calculateScore = (investor) => {
+    let sizeScore =
+      Math.abs(investor.investmentSize - smallBusiness.inventoryNeeds) / smallBusiness.inventoryNeeds;
+    let rateScore =
+      Math.abs(investor.interestRate - smallBusiness.preferredInterestRate) / smallBusiness.preferredInterestRate;
+    let timelineScore =
+      Math.abs(investor.timeline - smallBusiness.preferredTimeline) / smallBusiness.preferredTimeline;
+    
+    const totalScore =
+      (1 - sizeScore) * 0.4 + (1 - rateScore) * 0.3 + (1 - timelineScore) * 0.3;
+    return totalScore * 100;
+  };
+  
+
+  const handleNextLevel = () => {
+    if (levelIndex < initialLevels.length - 1) {
+      setLevelIndex(levelIndex + 1);
+      setSelectedInvestorStatus(Array(initialLevels[levelIndex + 1].potentialInvestors.length).fill(false));
+    } else {
+      handleGameOver();
+    }
+  };
+
+  const handleTimer = () => {
+    setTimeout(() => {
+      setTimer(timer - 1);
+      if (timer === 0) {
+        handleGameOver();
+      }
+    }, 1000);
+  };
+
+  const handleGameOver = () => {
+    setGameOver(true);
+  };
+
+  useEffect(() => {
+    const randomizeLevels = () => {
+      const shuffledInvestorNames = [...investorNames].sort(() => Math.random() - 0.5);
+      const shuffledBusinessNames = [...businessNames].sort(() => Math.random() - 0.5);
+
+      const randomizedLevels = levels.map((_, i) => {
+        const level = generateLevel(i);
+        return {
+          ...level,
+          smallBusiness: {
+              ...level.smallBusiness,
+              name: shuffledBusinessNames[i * 3 ],
+            },
+          potentialInvestors: level.potentialInvestors.map((investor, j) => {
+            return {
+              ...investor,
+              name: shuffledInvestorNames[i * 3 + j],
+            };
+          }),
+        };
+      });
+      setLevels(randomizedLevels);
+    };
+
+    randomizeLevels();
+  }, []);
+  
+  return (
+    <div className="min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12">
+      <div className="relative py-3 sm:max-w-xl sm:mx-auto">
+        <div className="absolute inset-0 bg-gradient-to-r from-cyan-400 to-light-blue-500 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl"></div>
+        <div className="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20">
+        <img src="/kickfurther-logo.png" alt="Kickfurther Logo" className="w-48 mx-auto mb-8" />
+          {gameOver ? (
+            <div className="text-center">
+              <h1 className="text-4xl font-bold text-black">Game Over!</h1>
+              <p className="text-xl mt-4 text-black">Your total score is: {Math.round(totalScore)}</p>
+            </div>
+          ) : (
+            <div>
+              <h1 className="text-4xl font-bold text-black">Kickfurther Matching Game</h1>
+              <div className="mt-4 text-black">
+                <p>Level: {levelIndex + 1}</p>
+                <p>Total Score: {Math.round(totalScore)}</p>
+              </div>
+              <h2 className="text-2xl font-semibold mt-6 text-black">Small Business Details</h2>
+              <div className="mt-4 text-black">
+                <p>Name: {smallBusiness.name}</p>
+                <p>Inventory Needs: ${smallBusiness.inventoryNeeds}</p>
+                <p>Preferred Interest Rate: {Math.round(smallBusiness.preferredInterestRate * 100)}%</p>
+                <p>Preferred Timeline: {smallBusiness.preferredTimeline} months</p>
+              </div>
+  
+              <h2 className="text-2xl font-semibold mt-6 text-black">Potential Investors</h2>
+              <div className="mt-4">
+                {potentialInvestors.map((investor, index) => (
+                  <div
+                    key={index}
+                    className={`p-6 rounded-lg shadow-md mb-4 ${selectedInvestorStatus[index] ? "bg-green-500" : "bg-black"}`}
+                  >
+                    <div className="text-white">
+                      <p>Name: {investor.name}</p>
+                      <p>Investment Size: ${investor.investmentSize}</p>
+                      <p>Interest Rate: {Math.round(investor.interestRate * 100)}%</p>
+                      <p>Timeline: {investor.timeline} months</p>
+                    </div>
+                    <button
+                      onClick={() => handleInvestorSelection(investor, index)}
+                      disabled={selectedInvestorStatus[index]}
+                      className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded mt-4"
+                    >
+                      Select Investor
+                    </button>
+                  </div>
+                ))}
+              </div>
+  
+              {/* <button onClick={handleNextLevel} className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded mt-4">
+                Next Level
+              </button> */}
+              {timer > 0 ? handleTimer() : handleGameOver()}
+
+              <div className="fixed bottom-8 right-8 p-4 bg-red-500 text-white rounded-lg shadow-md">
+                <p>Timer: {timer} seconds</p>
+              </div>
+              <div className="fixed bottom-8 left-8 p-8 bg-blue-500 text-white rounded-lg shadow-md" style={{ fontSize: "1.5rem", width: "250px" }}>
+                 <p>
+                  <strong>Total Score: {Math.round(totalScore)} </strong>
+                 </p>
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+  
+}
--- pages/index.js
+++ pages/index.js
@@ -1,163 +1,39 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from 'kfgame/styles/Home.module.css'
-import React, { useState, useEffect } from 'react';
-import { initialLevels, generateLevel, investorNames, businessNames } from '../components/levels';
-
-const inter = Inter({ subsets: ['latin'] })
-
+import Link from 'next/link';
+import Head from 'next/head';
 
 
 export default function Home() {
-  const [levelIndex, setLevelIndex] = useState(0);
-  const [levels, setLevels] = useState(initialLevels);
-  const [selectedInvestor, setSelectedInvestor] = useState();
-  const [totalScore, setTotalScore] = useState(0);
-  const [timer, setTimer] = useState(60); // Change the timer value based on your game requirements
-  const { smallBusiness, potentialInvestors } = levels[levelIndex];
-  const [selectedInvestorStatus, setSelectedInvestorStatus] = useState(
-    Array(potentialInvestors.length).fill(false)
-  );
-  const [gameOver, setGameOver] = useState(false);
-
-
-  const handleInvestorSelection = (investor) => {
-    setSelectedInvestor(investor);
-      const score = calculateScore(investor);
-      setTotalScore(totalScore + score);
-      handleNextLevel();
-  };
-
-   const calculateScore = (investor) => {
-    let sizeScore =
-      Math.abs(investor.investmentSize - smallBusiness.inventoryNeeds) /
-      smallBusiness.inventoryNeeds;
-    let rateScore =
-      Math.abs(investor.interestRate - smallBusiness.preferredInterestRate) /
-      smallBusiness.preferredInterestRate;
-    let timelineScore =
-      Math.abs(investor.timeline - smallBusiness.preferredTimeline) /
-      smallBusiness.preferredTimeline;
-  
-    const totalScore =
-      (1 - sizeScore) * 0.4 + (1 - rateScore) * 0.3 + (1 - timelineScore) * 0.3;
-    return totalScore * 100;
-  };
-  
-
-  const handleNextLevel = () => {
-    if (levelIndex < initialLevels.length - 1) {
-      setLevelIndex(levelIndex + 1);
-      setSelectedInvestorStatus(Array(initialLevels[levelIndex + 1].potentialInvestors.length).fill(false));
-    } else {
-      handleGameOver();
-    }
-  };
-
-  const handleTimer = () => {
-    setTimeout(() => {
-      setTimer(timer - 1);
-      if (timer === 0) {
-        handleGameOver();
-      }
-    }, 1000);
-  };
-
-  const handleGameOver = () => {
-    setGameOver(true);
-  };
-
-  useEffect(() => {
-    const randomizeLevels = () => {
-      const shuffledInvestorNames = [...investorNames].sort(() => Math.random() - 0.5);
-      const shuffledBusinessNames = [...businessNames].sort(() => Math.random() - 0.5);
-
-      const randomizedLevels = levels.map((_, i) => {
-        const level = generateLevel(i);
-        return {
-          ...level,
-          smallBusiness: {
-              ...smallBusiness,
-              name: shuffledBusinessNames[i * 3 ],
-            },
-          potentialInvestors: level.potentialInvestors.map((investor, j) => {
-            return {
-              ...investor,
-              name: shuffledInvestorNames[i * 3 + j],
-            };
-          }),
-        };
-      });
-      setLevels(randomizedLevels);
-    };
-
-    randomizeLevels();
-  }, []);
-  
   return (
-    <div className="min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12">
-      <div className="relative py-3 sm:max-w-xl sm:mx-auto">
-        <div className="absolute inset-0 bg-gradient-to-r from-cyan-400 to-light-blue-500 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl"></div>
-        <div className="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20">
-        <img src="/kickfurther-logo.png" alt="Kickfurther Logo" className="w-48 mx-auto mb-8" />
-          {gameOver ? (
-            <div className="text-center">
-              <h1 className="text-4xl font-bold text-black">Game Over!</h1>
-              <p className="text-xl mt-4 text-black">Your total score is: {Math.round(totalScore)}</p>
-            </div>
-          ) : (
-            <div>
-              <h1 className="text-4xl font-bold text-black">Kickfurther Matching Game</h1>
-              <div className="mt-4 text-black">
-                <p>Level: {levelIndex + 1}</p>
-                <p>Total Score: {Math.round(totalScore)}</p>
-              </div>
-              <h2 className="text-2xl font-semibold mt-6 text-black">Small Business Details</h2>
-              <div className="mt-4 text-black">
-                <p>Name: {smallBusiness.name}</p>
-                <p>Inventory Needs: ${smallBusiness.inventoryNeeds}</p>
-                <p>Preferred Interest Rate: {Math.round(smallBusiness.preferredInterestRate * 100)}%</p>
-                <p>Preferred Timeline: {smallBusiness.preferredTimeline} months</p>
-              </div>
-  
-              <h2 className="text-2xl font-semibold mt-6 text-black">Potential Investors</h2>
-              <div className="mt-4">
-                {potentialInvestors.map((investor, index) => (
-                  <div
-                    key={index}
-                    className={`p-6 rounded-lg shadow-md mb-4 ${selectedInvestorStatus[index] ? "bg-green-500" : "bg-black"}`}
-                  >
-                    <div className="text-white">
-                      <p>Name: {investor.name}</p>
-                      <p>Investment Size: ${investor.investmentSize}</p>
-                      <p>Interest Rate: {Math.round(investor.interestRate * 100)}%</p>
-                      <p>Timeline: {investor.timeline} months</p>
-                    </div>
-                    <button
-                      onClick={() => handleInvestorSelection(investor, index)}
-                      disabled={selectedInvestorStatus[index]}
-                      className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded mt-4"
-                    >
-                      Select Investor
-                    </button>
-                  </div>
-                ))}
-              </div>
-  
-              {/* <button onClick={handleNextLevel} className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded mt-4">
-                Next Level
-              </button> */}
-              {timer > 0 ? handleTimer() : handleGameOver()}
-
-              <div className="fixed bottom-8 right-8 p-4 bg-red-500 text-white rounded-lg shadow-md">
-                <p>Timer: {timer} seconds</p>
-              </div>
-            </div>
-          )}
+ <div className="min-h-screen flex flex-col justify-center items-center" style={{ backgroundImage: 'url(https://media.discordapp.net/attachments/1051763284946210870/1090197121250119790/n8r8t_As_a_growing_business_you_have_successfully_secured_inven_2428807e-4cdc-456d-be90-bd386ef95fe2.png?width=2050&height=1366)', backgroundSize: 'cover', backgroundPosition: 'center'}}>
+        <Head>
+        <meta name="description" content="Test your matching skills by matching investors with small businesses and earn points in the process!" />
+      </Head>
+  <div className="bg-white bg-opacity-75 p-8 rounded-lg">
+    <img src="/kickfurther-logo.png" alt="Kickfurther Logo" className="w-48 mx-auto mb-8" />
+    <h1 className="text-4xl font-bold text-black mb-4">Welcome to the Kickfurther Matching Game</h1>
+    <p className="text-lg text-black mb-8">
+      Test your matching skills by matching investors with small businesses and earn points in the process!
+    </p>
+    <Link href="/game">
+    <div className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded cursor-pointer text-center">
+          Start Playing
         </div>
-      </div>
-    </div>
+  </Link>
+  <div className="absolute bottom-8 right-8 p-4 bg-blue-500 text-white rounded-lg shadow-md">
+    <p className="font-bold">
+      Game Rules:
+    </p>
+    <ul className="list-disc pl-4 mt-2">
+      <li>The game starts with a small business looking to secure financing for their inventory. The business will provide some details about their business and inventory needs.</li>
+      <li>Players will be presented with a list of potential investors on Kickfurther, along with their investment criteria.</li>
+      <li>The player’s goal is to match the small business with the most suitable investor(s) on Kickfurther. To do this, the player will need to read through the investment criteria and select the investors that are the best match for the small business.</li>
+      <li>Once the player has made their selection, they will be given a score based on how well they matched the small business with the investors. The score will be based on factors such as the investor’s investment size, their interest rate, and their investment timeline.</li>
+      <li>The game will end once the player has completed all levels or has failed to match the small business with the investors in the allotted time.</li>
+    </ul>
+  </div>
+  </div>
+</div>
+
+
   );
-  
 }
